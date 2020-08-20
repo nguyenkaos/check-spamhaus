4a8caab1ab2dfaac00d0905a30dcba862a790be4
pinkLotus <nguyen.caal@gmail.com>
2020-08-20 15:55:12 +0700

    Update check_spamhaus.py

--- check_spamhaus.py
+++ check_spamhaus.py
@@ -2,30 +2,32 @@ import requests
 import re
 
 
-def check_spamhaus_method1(domain):
-    r_auth = requests.get('https://mxtoolbox.com/api/v1/user')
-    TempAuthKey = r_auth.json()['TempAuthKey']
-    payload = { 
-        "accept":"application/json, text/javascript, */*; q=0.01",
-        "accept-encoding":"gzip, deflate, br",
-        "accept-language":"fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7",
-        "content-type":"application/json; charset=utf-8", 
-        "referer":"https://mxtoolbox.com/SuperTool.aspx?action=blacklist%3a{}&run=toolpage".format(domain),
-        "sec-fetch-mode":"cors",
-        "sec-fetch-site":"same-origin",
-        "tempauthorization":TempAuthKey,
-        "user-agent":"Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36",
-        "x-requested-with": "XMLHttpRequest",
-    }
-     
-    url="https://mxtoolbox.com/api/v1/Lookup?command=blacklist&argument={}&resultIndex=2&disableRhsbl=true&format=2".format(domain)
-    r = requests.get(url, headers=payload)
-    data = r.json()
-    index1 = data['HTML_Value'].find('Spamhaus DBL')
-    if 'was listed' in data['HTML_Value'][index1:(index1+150)]:
-        return False
-    else:
-        return True
+def check_spamhaus_method1(domain): 
+    token_list = [
+        '1d30737e-971f-45a8-af52-40d022c80240',
+        '4fd1da4c-76a0-4222-ab98-1890aca21b6e',
+        'dab115f4-48c6-4ac8-95fd-72c0ef9f6d25',
+        '1379f99e-5556-4ad6-9055-4db63c70463e',
+        '49ae5869-59e8-480d-ace5-f92b4a4b8f9a',
+        'e49e6bde-76dd-4b8c-9890-d57fa477a41d',
+    ]
+    import random
+    random.shuffle(token_list)
+    for token in token_list:
+        print('[check_spamhaus_method1] use token %s' % token)
+        url = 'https://api.mxtoolbox.com/api/v1/Lookup/blacklist/?argument={domain}&Authorization={token}'
+        url = url.format(domain=domain, token=token)
+        r = requests.get(url)
+        print('[check_spamhaus_method1] status code: %s' % r.status_code)
+        if r.status_code < 300:
+            for x in r.json()['Failed']:
+                if 'spamhaus dbl' in x['Name'].lower():
+                    print('bad')
+                    return False 
+            print('good')
+            return True
+    return None
+
 
 def check_spamhaus_method2(domain):
     url = "http://multirbl.valli.org/lookup/"+str(domain)+".html"
@@ -37,20 +39,22 @@ def check_spamhaus_method2(domain):
     else:
         return True
 
+
 def check_spamhaus(domain):
     """ 
     Return True if domain is good
            False if domain is bad
            None otherwise
     """
+    print('Check Spamhaus for: %s' % domain)
     try:
         return check_spamhaus_method1(domain)
-    except:
-        pass
+    except Exception as e:
+        print('ERR check_spamhaus_method1: %s' % e)
     try:
         return check_spamhaus_method2(domain)
-    except:
-        pass
+    except Exception as e:
+        print('ERR check_spamhaus_method2: %s' % e)
     return None
 
 
